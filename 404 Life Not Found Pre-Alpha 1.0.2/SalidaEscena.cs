faa1c236bd4ffbb37be007afbb40b13a8dda4904
Isabelle Martinez <78103738+isabellemartinez@users.noreply.github.com>
2026-07-14 13:11:59 -0500

    Update SalidaEscena.cs

--- 404 Life Not Found Pre-Alpha 1.0.2/SalidaEscena.cs
+++ 404 Life Not Found Pre-Alpha 1.0.2/SalidaEscena.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SalidaEscena : MonoBehaviour
 {
+    [Header("Transición de escena")]
     [SerializeField] private string escenaDestino;
     [SerializeField] private string spawnDestino;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
 
         SpawnManager.spawnObjetivo = spawnDestino;
         SceneManager.LoadScene(escenaDestino);
